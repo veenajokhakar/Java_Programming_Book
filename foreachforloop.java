46927d328386bc35a65a3ce4f26fc77e05f7044f
veenajokhakar <44340107+veenajokhakar@users.noreply.github.com>
2020-06-16 19:49:21 -0700

    Add files via upload

---
+++ foreachforloop.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class foreachforloop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class foreachforloop
+{
+   public static void main(String[] args) {  
+    //Declaring an array  
+    int arr[]={12,55,66,56,48};  
+    //Printing array using for-each loop  
+    for(int i:arr){  
+        System.out.println(i);  
+    }  
+}  
+}
