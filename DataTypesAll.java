05c2b36fe258ad4515d6c15c94013930daf6b22f
veenajokhakar <44340107+veenajokhakar@users.noreply.github.com>
2020-06-16 19:33:30 -0700

    Add files via upload

---
+++ DataTypesAll.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class DataTypesAll here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DataTypesAll
+{
+ public static void main(String[] args){
+    byte myNumb = 100;
+System.out.println(myNumb);
+
+short myNums = 5000;
+System.out.println(myNums);
+
+int myNumi = 100000;
+System.out.println(myNumi);
+
+long myNuml = 15000000000L;
+System.out.println(myNuml);
+
+float myNumf = 5.75f;
+System.out.println(myNumf);
+
+double myNumd = 19.99d;
+System.out.println(myNumd);
+
+    }
+}
