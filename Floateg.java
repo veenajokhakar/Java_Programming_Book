2715247a584813bddb62428afbbab92d093fa477
veenajokhakar <44340107+veenajokhakar@users.noreply.github.com>
2020-06-16 19:37:59 -0700

    Add files via upload

---
+++ Floateg.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Floateg here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Floateg
+{
+   public static void main(String[] args){
+    float f = 25e3f;
+double d = 13E4d;
+System.out.println(f);
+System.out.println(d);
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);     // Outputs true
+System.out.println(isFishTasty);   // Outputs false
+//character
+char myGrade = 'B';
+System.out.println(myGrade);
+
+    }
+}
